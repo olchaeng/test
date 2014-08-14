e02078cd3f7da6521ea3d77be674e29f3583d014
Jung Gwang-yun <jky0207@naver.com>
2014-08-14 05:05:13 +0000

    asdf

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var qs = require('querystring');
+
 http.createServer(function(request, response) {
- response.writeHead(200, {'Content-Type': 'text/html'});
-response.end('<h1>~~~</h1>');
+  if (request.method == 'POST') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+
+            console.log(post);
+            // use post['blah'], etc.
+        });
+    }
 }).listen(52273, function() {
-console.log('Server running at http://127.0.0.1:52273/');
+  console.log('Server running at http://127.0.0.1:52273/');
 });
