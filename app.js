48cdd1e2b45e0acf56895ffb740453d18c2cc21a
Jung Gwang-yun <jky0207@naver.com>
2014-08-14 05:30:11 +0000

    mysql test

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var http = require('http');
 var qs = require('querystring');
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'amdin',
+  password : 'admin',
+  database : 'movie_app'
+});
+
+connection.connect();
+ 
+var queryString = 'SELECT * FROM member';
+ 
+connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+ 
+    for (var i in rows) {
+        console.log('Post Titles: ', rows[i].post_title);
+    }
+});
+ 
+connection.end();
+
 http.createServer(function(request, response) {
   if (request.method == 'POST') {
         var body = '';
@@ -16,9 +38,11 @@ http.createServer(function(request, response) {
 
             console.log(post);
             // use post['blah'], etc.
+            response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+            response.end();
         });
     }
-    response.end();
+    
 }).listen(52273, function() {
   console.log('Server running at http://127.0.0.1:52273/');
 });
