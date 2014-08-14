238389a6649dbaa146a147d8def705c7edd6cfcf
Jung Gwang-yun <jky0207@naver.com>
2014-08-14 05:10:46 +0000

    asdf

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ http.createServer(function(request, response) {
             // use post['blah'], etc.
         });
     }
+    response.end();
 }).listen(52273, function() {
   console.log('Server running at http://127.0.0.1:52273/');
 });
